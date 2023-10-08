8ebec4e852babee54d98fa86c405236600b0d7d2
Corey Veitch-McAllister <corey_mac@hotmail.co.uk>
2023-10-08 20:16:53 +0000

    Added Asset Folders & Depedencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,19 @@
 import "package:flutter/material.dart";
-import "pages/homepage.dart";
-import "pages/aboutme.dart";
-import 'pages/Contact.dart';
-import "pages/numberGen.dart";
+import 'package:audioplayers/audioplayers.dart';
+
 
 void main() {
   runApp(const MyApp()); //MyApp
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int _currentIndex = 0;
-  List<Widget> pages = [HomePage(), AboutMe()];
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (int index){
-            setState(() {
-              _currentIndex= index;
-            });
-          },
-          items: [
-        BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
-        BottomNavigationBarItem(label: "ABC", icon: Icon(Icons.abc)),
-      ]),
-      body: IndexedStack(index: _currentIndex,
-      children: pages),
+    return MaterialApp(home:Scaffold(
+      appBar: AppBar(title: Text("Home Page"), centerTitle: true,),
+      body: Text("Hello World"),
     ));
   }
 }
