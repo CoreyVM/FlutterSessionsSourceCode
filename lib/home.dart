cbc4f9102f631bf2649114dcc360c497761a6875
Corey Veitch-McAllister <corey_mac@hotmail.co.uk>
2023-10-17 15:26:32 +0100

    qwe,e.nwelk

---
+++ lib/home.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+
+Container MyProp(String val) {
+  return Container(
+    margin: const EdgeInsets.all(50),
+    child: Text(val),
+  );
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,120 @@
 import "package:flutter/material.dart";
-import 'package:audioplayers/audioplayers.dart';
-
 
 void main() {
-  runApp(const MyApp()); //MyApp
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home:Scaffold(
-      appBar: AppBar(title: Text("Home Page"), centerTitle: true,),
-      body: ElevatedButton(onPressed: (){
-        AudioPlayer audio = AudioPlayer();
-        audio.play(UrlSource("lib/assets/audio/cat.mp3"));
-      },child: Text("help"),),
-    ));
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Nike"),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+           child: Column(
+              children: [
+                
+                // Search Bar
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.search),
+                        hintText: "Search",
+                      ),
+                    ),
+                  ),
+                ),
+                // Categories
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: Text("Categories", style: TextStyle(fontSize: 22)),
+                  ),
+                ),
+                // Categories Row
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(height: 100, width: 100, color: Colors.red),
+                      Container(height: 100, width: 100, color: Colors.blue),
+                      Container(height: 100, width: 100, color: Colors.green),
+                    ],
+                  ),
+                ),
+                // Popular
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Popular", style: TextStyle(fontSize: 22)),
+                  ),
+                ),
+                // Popular Row
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(height: 100, width: 100, color: Colors.orange),
+                      Container(height: 100, width: 100, color: Colors.black),
+                      Container(height: 100, width: 100, color: Colors.yellow),
+                    ],
+                  ),
+                ),
+                // On Demand
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "On Demand",
+                        style: TextStyle(fontSize: 22, decoration: TextDecoration.underline),
+                      ),
+                    ),
+                  ),
+                ),
+                // On Demand Row
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(height: 150, width: 150, color: Colors.orange),
+                      Container(height: 150, width: 150, color: Colors.orange),
+                    ],
+                  ),
+                ),
+                Text("On Sale", style: TextStyle(fontSize: 24)),
+                // Wrap widget to wrap the Containers
+                // ...
+                Expanded(child: Text("On Sale", style: TextStyle(fontSize: 24))),
+                Expanded(
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      Container(height: 80, width: 80, color: Colors.red),
+                      Container(height: 80, width: 80, color: Colors.yellow),
+                      Container(height: 80, width: 80, color: Colors.blue),
+                      Container(height: 80, width: 80, color: Colors.blue),
+                      Container(height: 80, width: 80, color: Colors.blue),
+                      Container(height: 80, width: 80, color: Colors.blue),
+                      Container(height: 80, width: 80, color: Colors.blue),
+                      Container(height: 80, width: 80, color: Colors.yellow),
+                      Container(height: 80, width: 80, color: Colors.green),
+                    ],
+                  ),
+                ),
+            //   Add some space at the end to allow scrolling
+              ],
+            ),
+          ),
+
+        ),
+      );
   }
 }
